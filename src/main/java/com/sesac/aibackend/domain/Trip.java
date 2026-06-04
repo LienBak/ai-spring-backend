ff1c78cc2c331fb55341fd2ed86980c2d6d5a1cd
gahui22 <gahui22@naver.com>
2026-06-04 15:56:22 +0900

    trip 도메인 객체

---
+++ src/main/java/com/sesac/aibackend/domain/Trip.java
@@ -0,0 +1,16 @@
+package com.sesac.aibackend.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Trip {
+
+    private Long id;
+    private String title;
+    private String departure;
+    private String destination;
+}
