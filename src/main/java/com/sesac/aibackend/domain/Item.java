9d156c04e444168a4b142e119c40fb67134b4320
gahui22 <gahui22@naver.com>
2026-06-05 16:41:13 +0900

    환경설정

--- src/main/java/com/sesac/aibackend/domain/Item.java
+++ src/main/java/com/sesac/aibackend/domain/Item.java
@@ -1,5 +1,6 @@
 package com.sesac.aibackend.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,9 +8,17 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder        // 객체를 만들어주는?
+@Entity
+@Table(name = "items")
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false)
     private int price;
 }
